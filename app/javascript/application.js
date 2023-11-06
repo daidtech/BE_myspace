0c38619c96792ffd0243b4d04dcdd9e92c60e98a
daid <dvdai63@gmail.com>
2023-11-06 21:12:25 +0700

    update UI

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,27 +5,29 @@ import 'jquery'
 import 'jquery_ujs'
 import 'bootstrap'
 
-const updateMenuBorderPosition = (top) => {
-  $('#left__sidebar').css({top: top+'px'})
-}
-
-const changeActiveTab = (e) => {
-  const cus_nav_items = $('.cus-nav-item')
-  for (const nav of cus_nav_items) {
-    $(nav).removeClass('active')
+$( document ).on('turbolinks:load', function() {
+  const updateMenuBorderPosition = (top) => {
+    $('#left__sidebar').css({top: top+'px'})
   }
-  $(`#${e.target.id}`).addClass('active')
-  updateMenuBorderPosition($(`#${e.target.id}`).position().top - 45)
-}
 
-const handleClickNav = (e) => {
-  e.preventDefault()
-  changeActiveTab(e)
-}
-
-window.addEventListener("load", () => {
-  const cus_nav_items = $('.cus-nav-item')
-  for (const nav of cus_nav_items) {
-    nav.addEventListener("click", (e) => handleClickNav(e))
+  const changeActiveTab = (e) => {
+    const cus_nav_items = $('.cus-nav-item')
+    for (const nav of cus_nav_items) {
+      $(nav).removeClass('active')
+    }
+    $(`#${e.target.id}`).addClass('active')
+    updateMenuBorderPosition($(`#${e.target.id}`).position().top - 45)
   }
+
+  const handleClickNav = (e) => {
+    e.preventDefault()
+    changeActiveTab(e)
+  }
+
+  window.addEventListener("load", () => {
+    const cus_nav_items = $('.cus-nav-item')
+    for (const nav of cus_nav_items) {
+      nav.addEventListener("click", (e) => handleClickNav(e))
+    }
+  })
 })

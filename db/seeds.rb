49c634306421fcd926edf8026a91d644df9ae6e6
daid <dvdai63@gmail.com>
2024-03-01 08:26:58 +0700

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# AboutPage
+name: Damas
+introduce: `a Ruby on Rails developer with more than 4 years of experience developing websites.
+            I have a good understanding of database design and high load balancing. I enjoy solving problems.
+            I enjoy exercising and tracking my habits to improve them over time.
+            I believe that anyone can become talented if they believe in themselves.
+            I enjoy reading books and finding new ideas from them.
+            My coworkers say that I am a good team player and that I help keep the team happier and
+            less stressed when a release is coming.`
+skills:
+  title: 'PROGRAMMING LANGUAGES'
+  details: ['Ruby', 'HTML, CSS', 'JavaScript']
+  title: 'FRAMEWORKS & PLATFORMS'
+  details: ['ReactJS/Vuejs', 'Ruby on Rails', 'React Bootstrap']
